8187274785afe71632d3d371ff21e823371ccaa6
moanes <mohamed.moanes@easyschools.org>
2021-11-08 16:39:57 +0200

    update api end point

--- app/src/main/java/com/moanes/flickrapp/data/network/Service.kt
+++ app/src/main/java/com/moanes/flickrapp/data/network/Service.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Service {
-    @GET("rest/?format=json&nojsoncallback=1&method=flickr.photos.getRecent")
+    @GET("rest/?format=json&nojsoncallback=1&method=flickr.photos.search&tags=[trending,popular]")
     suspend fun getPhotos(
         @Query("per_page") perPage: Int,
         @Query("page") page: Int
