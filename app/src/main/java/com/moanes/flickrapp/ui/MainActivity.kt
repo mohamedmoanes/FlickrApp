95964be9fa19c8f526ba02bf9d42f5dc576b47b3
mohamed moanes <mohamed.moanes@easyschools.org>
2021-11-09 00:36:56 +0200

    handling back button

--- app/src/main/java/com/moanes/flickrapp/ui/MainActivity.kt
+++ app/src/main/java/com/moanes/flickrapp/ui/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.moanes.flickrapp.ui
 
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.moanes.flickrapp.R
 import com.moanes.flickrapp.base.BaseActivity
 
 class MainActivity : BaseActivity() {
+    private lateinit var navController: NavController
+
     override fun getLayout(): Int {
         return R.layout.activity_main
     }
 
+
+    private fun initNavController() {
+        navController = findNavController(R.id.auth_host_fragment)
+    }
+
     override fun init() {
+        initNavController()
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination?.id == R.id.photosListFragment)
+            super.onBackPressed()
+        else
+            navController.navigateUp()
     }
 }
